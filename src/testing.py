8a3794ca8e152f88e8d65eeada7147f2a12657ea
panwayas <yashraj16.panwar@gmail.com>
2025-04-05 23:30:38 -0700

    tests audio

--- src/testing.py
+++ src/testing.py
@@ -6,22 +6,26 @@ import file_handler
 class Main:
     @staticmethod
     def run():
-        db = VectorDB()
-        image_desc = file_handler.process_image_file("/Users/yashpanwar/Downloads/sunflower.jpg")
-        text_desc = file_handler.process_text_file("/Users/yashpanwar/Downloads/catinthehat.txt") #/Users/yashpanwar/Downloads/dialog.txt
-        #pdf_desc = file_handler.process_pdf_file("'/Users/yashpanwar/Downloads/Ch 2 Test outline.pdf'")
-        csv_desc = file_handler.process_csv_file("/Users/yashpanwar/git/expressionevaluator-aden-panwayas-mvla/bin/analysis_sum.csv")
-        test_documents = [
-            image_desc,
-            text_desc,
-            csv_desc
-        ]
-        db.add_documents(test_documents)
-        tree_context = """
-        Picture with some nice scenery, looks like a flower with vibrancy.
-        """
-        best_doc = db.search_with_context(tree_context)
-        print(f"\nBest Question for context: {best_doc['document']}")
+        text_audio = file_handler.process_initial_audio("/Users/yashpanwar/Downloads/rick.mp3")
+        print(text_audio)
+        # db = VectorDB()
+        # image_desc = file_handler.process_image_file("/Users/yashpanwar/Downloads/sunflower.jpg")
+        # text_desc = file_handler.process_text_file("/Users/yashpanwar/Downloads/catinthehat.txt") #/Users/yashpanwar/Downloads/dialog.txt
+        # #pdf_desc = file_handler.process_pdf_file("'/Users/yashpanwar/Downloads/Ch 2 Test outline.pdf'")
+        # csv_desc = file_handler.process_csv_file("/Users/yashpanwar/git/expressionevaluator-aden-panwayas-mvla/bin/analysis_sum.csv")
+        # test_documents = [
+        #     image_desc,
+        #     text_desc,
+        #     csv_desc
+        # ]
+        # db.add_document(test_documents[0])
+        # db.add_document(test_documents[1])
+
+        # tree_context = """
+        # Picture with some nice scenery, looks like a flower with vibrancy.
+        # """
+        # best_doc = db.search_with_context(tree_context)
+        # print(f"\nBest Question for context: {best_doc['document']}")
 
 
 if __name__ == '__main__':
